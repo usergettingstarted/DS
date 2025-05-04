307528ca1a2411b4b3e96b9c7ee129241986173c
usergettingstarted <usergettingstarted@gmail.com>
2025-05-05 02:56:37 +0530

    Update Client.java

--- A1/Client.java
+++ A1/Client.java
@@ -7,14 +7,14 @@ public class Client {
 		try {
 			String serverURL = "rmi://localhost/Server";
 			ServerIntf serverIntf = (ServerIntf) Naming.lookup(serverURL);
-      System.out.print("Enter a year to check if it’s a leap year: ");
-      int year = sc.nextInt();
-      boolean isLeap = serverIntf.isLeapYear(year);
-      if (isLeap) {
-        System.out.println(year + " is a leap year.");
-      } else {
-        System.out.println(year + " is not a leap year.");
-      }
+      			System.out.print("Enter a year to check if it’s a leap year: ");
+      			int year = sc.nextInt();
+      			boolean isLeap = serverIntf.isLeapYear(year);
+      			if (isLeap) {
+        			System.out.println(year + " is a leap year.");
+      			} else {
+        			System.out.println(year + " is not a leap year.");
+      			}
 		}
 		catch(Exception e) {
 			System.out.println("Exception Occured at Client "+e.getMessage());
